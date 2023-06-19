7107f466193a5657abd525e41da70a2c4fb81130
KaterinaPy35 <yakushka1308@mail.ru>
2023-06-19 23:11:39 +0300

    Changed imports in base.html, deleted mypy.ini, added mypy configurations in pyproject.toml, reinstalled packages, added urls.py in core application

--- src/jobboard_app/jobboard_app/urls.py
+++ src/jobboard_app/jobboard_app/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from core import views
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('', views.index),
-    path('add/', views.add),
-    path('companies/', views.companies),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
